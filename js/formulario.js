4c8e1ff8f4b387d0a77b50e0d04cbd5e66036bac
NicolasdosSantos <cabritanicolas@hotmail.com>
2022-07-25 18:46:07 -0300

    formulario terminado

--- js/formulario.js
+++ js/formulario.js
@@ -10,6 +10,14 @@ const expresiones = {
 	telefono: /^\d{7,14}$/ // 7 a 14 numeros.
 }
 
+const campos = {
+    usuario: false,
+    nombre: false,
+    password: false,
+    mail: false,
+    telefono: false
+}
+
 const validarFormulario = (e) => {
     switch (e.target.name) {
         case "usuario":
@@ -20,6 +28,7 @@ const validarFormulario = (e) => {
         break;
         case "password":
             validarCampo(expresiones.password, e.target, 'password');
+            validarPassword2();
         break;
         case "password2":
             validarPassword2();
@@ -40,12 +49,14 @@ const validarCampo = (expresion, input, campo) => {
         document.querySelector(`#grupo_${campo} i`).classList.add('fa-check-circle');
         document.querySelector(`#grupo_${campo} i`).classList.remove('fa-times-circle');
         document.querySelector(`#grupo_${campo} .formulario_input_error`).classList.remove('formulario_input_error-activo');
+        campos[campo] = true;
     } else {
         document.getElementById(`grupo_${campo}`).classList.add('formulario_grupo-incorrecto');
         document.getElementById(`grupo_${campo}`).classList.remove('formulario_grupo-correcto');
         document.querySelector(`#grupo_${campo} i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo_${campo} i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo_${campo} .formulario_input_error`).classList.add('formulario_input_error-activo');
+        campos[campo] = false;
     }
 }
 
@@ -59,12 +70,14 @@ const validarPassword2 = () => {
         document.querySelector(`#grupo_password2 i`).classList.add('fa-times-circle');
         document.querySelector(`#grupo_password2 i`).classList.remove('fa-check-circle');
         document.querySelector(`#grupo_password2 .formulario_input_error`).classList.add('formulario_input_error-activo');
+        campos['password'] = false;
     }else {
         document.getElementById(`grupo_password2`).classList.remove('formulario_grupo-incorrecto');
         document.getElementById(`grupo_password2`).classList.add('formulario_grupo-correcto');
         document.querySelector(`#grupo_password2 i`).classList.remove('fa-times-circle');
         document.querySelector(`#grupo_password2 i`).classList.add('fa-check-circle');
         document.querySelector(`#grupo_password2 .formulario_input_error`).classList.add('formulario_input_error-activo');
+        campos['password'] = true;
     }
 }
 
@@ -76,5 +89,21 @@ inputs.forEach((input) => {
 
 formulario.addEventListener('submit', (e) => {
     e.preventDefault();
+
+    const terminos = document.getElementById('terminos');
+    if(campos.usuario && campos.nombre && campos.password && campos.mail && campos.telefono && terminos.checked) {
+        formulario.reset();
+
+        document.getElementById('formulario_mensaje-exito').classList.add('formulario_mensaje-exito-activo');
+        setTimeout(() => {
+            document.getElementById('formulario_mensaje-exito').classList.remove('formulario_mensaje-exito-activo');
+        }, 3000);
+
+        document.querySelectorAll('.formulario_grupo-correcto').forEach((icono) => {
+            icono.classList.remove('formulario_grupo-correcto');
+        });
+    } else {
+        document.getElementById('formulario_mensaje').classList.add('formulario_mensaje-activo')
+    }
 });
 
